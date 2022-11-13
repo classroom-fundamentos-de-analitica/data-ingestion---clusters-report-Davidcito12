bde90792ddbb2b808c04341af91fa4a9e7141ff8
David Lopez <92931033+Davidcito12@users.noreply.github.com>
2022-11-12 22:08:32 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,48 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
-
+import re
 def ingest_data():
+    with open('clusters_report.txt') as text:
+        row = text.readlines()
 
-    #
-    # Inserte su código aquí
-    #
+    row = row[4:]
+    for i in range(len(row)):
+        row[i] = row[i].strip()
 
-    return df
+    categories = ''
+    numeros = []
+    cantidades =[]
+    porcentajes = []
+    for i in row:
+
+        try:
+            int((i[0]+i[1]))
+            numero, cantidad, porcentaje, basura, *palabras = i.split()
+            numeros.append(int(numero))
+            porcentajes.append(float(re.sub("[,]",".",porcentaje)))
+            cantidades.append(int(cantidad))
+            palabras[0] = "-x-x-x-x-" + palabras[0]
+            for palab in palabras:
+                categories += " " + palab
+        except:
+            if i == "":
+                continue
+            k = i.split()
+            k[0] = " " + k[0]
+            for palab in k:
+                categories += "  " + palab
+
+    categories = re.sub(" +"," ",categories)
+    categories = re.sub("[.]","",categories)
+    categories = categories.split('-x-x-x-x-')
+    categories = list(map(lambda x: x.strip(), categories))
+    del(categories[0])
+
+    dataframe = pd.DataFrame()
+    dataframe['cluster'] = numeros
+    dataframe['cantidad_de_palabras_clave'] = cantidades
+    dataframe['porcentaje_de_palabras_clave'] = cantidades
+    dataframe['principales_palabras_clave'] = categories
+
+    return dataframe
